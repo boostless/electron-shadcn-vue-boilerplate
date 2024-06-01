7e04af698a64599bb5947c3411f2e92bde0dfc2a
boostless <jonas68824@gmail.com>
2024-06-01 20:25:56 +0300

    init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+</script>
+
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <div class="flex items-center space-x-2">
+      <Switch id="electron-shadcn" />
+      <Label for="electron-shadcn">This is awesome !</Label>
+    </div>
+  </div>
+</template>
